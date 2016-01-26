1be2f7c61df2b4d9b4e7e68f9defffc983cae6b4
Varinder Singh <vicky98284@gmail.com>
2016-01-26 15:52:32 +0530

    1.Add app.sh as a thin wrapper to set command line options. 2.Add Procfile to use heroku-style process model. 3.Some cleanup of bootstrap.yml

--- app.sh
+++ app.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
-CATALINA_OPTS="-Xms256m -Xmx2G"
-CATALINA_OPTS="$CATALINA_OPTS -Xss256k"
-CATALINA_OPTS="$CATALINA_OPTS -XX:ThreadPriorityPolicy=42"
-CATALINA_OPTS="$CATALINA_OPTS -XX:MaxGCPauseMillis=150"
+# Author: Varinder Singh
+# Description: This script acts as thin wrapper and can be used to set command line flags
+# to JVM or CATALINA
 
-CATALINA_OPTS="$CATALINA_OPTS -server"
-CATALINA_OPTS="$CATALINA_OPTS -XX:+DisableExplicitGC"
+: "${DISCOVERY_SERVICE_HOST:? is not set.Exiting...}"
+: "${DISCOVERY_SERVICE_PORT:=8001}"
+
+#CATALINA_OPTS="-Xms64m -Xmx128m"
+#CATALINA_OPTS="$CATALINA_OPTS -Xss256k"
+#CATALINA_OPTS="$CATALINA_OPTS -XX:ThreadPriorityPolicy=42"
+#CATALINA_OPTS="$CATALINA_OPTS -XX:MaxGCPauseMillis=150"
+
+#CATALINA_OPTS="$CATALINA_OPTS -server"
+#CATALINA_OPTS="$CATALINA_OPdTS -XX:+DisableExplicitGC"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-JAR_FILE="replace me"
+JAR_FILE="target/*.jar"
 
-LISTEN_PORT=${PORT:-5000}
+SPRING_CMD_OPTS="--eureka.client.serviceUrl.defaultZone=http://${DISCOVERY_SERVICE_HOST}:${DISCOVERY_SERVICE_PORT}/eureka/"
+
+echo $SPRING_CMD_OPTS
 
 # http://docs.oracle.com/middleware/1213/wls/WLSRN/issues.htm#BCFFDBEB
 java -Djava.security.egd=file:/dev/./urandom \
 $CATALINA_OPTS -jar $DIR/$JAR_FILE \
---server.port=$LISTEN_PORT --server.tomcat.uri-encoding=UTF-8
+$SPRING_CMD_OPTS
